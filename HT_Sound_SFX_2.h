d78d3b6360810db5f8bf80ada4d166d9bcc28bd9
gsemiceki7-ship-it <gsemiceki7@gmail.com>
2026-05-16 22:25:18 -0300

    Add HT_Sound_SFX_2.h with Music Events, NPC, and Object/Pickup sound codes

---
+++ HT_Sound_SFX_2.h
@@ -0,0 +1,164 @@
+#pragma once
+
+/**
+ * SFX Hash Type (HT) constants - Part 2
+ * EuroSound Editor EngineX
+ * Ice Age 2: The Meltdown v2.80
+ * 
+ * Additional sound effect hash codes organized by category:
+ * - Music Event sounds
+ * - NPC (Non-Player Character) sounds
+ * - Object and Pickup sounds
+ */
+
+/* ===== MUSIC EVENT SOUNDS ===== */
+#define HT_Sound_SFX_MUS_EVENT_MAJOR                    0x2D7001DB
+#define HT_Sound_SFX_MUS_EVENT_MINOR                    0x2D7001DC
+
+/* ===== NPC SOUNDS - CONDOR CHICK ===== */
+#define HT_Sound_SFX_NPC_CONDORCHICK_FALL               0x2D70063B
+#define HT_Sound_SFX_NPC_CONDORCHICK_IDLE_SCRATCH       0x2D7000FC
+#define HT_Sound_SFX_NPC_CONDORCHICK_RESPAWN            0x2D70063C
+#define HT_Sound_SFX_NPC_CONDORCHICK_STEP_L             0x2D7000FA
+#define HT_Sound_SFX_NPC_CONDORCHICK_STEP_R             0x2D7000FB
+#define HT_Sound_SFX_NPC_CONDORCHICK_TAKEHIT            0x2D70018E
+
+/* ===== NPC SOUNDS - CONDOR ===== */
+#define HT_Sound_SFX_NPC_CONDOR_CALL                    0x2D700184
+#define HT_Sound_SFX_NPC_CONDOR_CALL_SHORT              0x2D700183
+#define HT_Sound_SFX_NPC_CONDOR_WING_FLAP               0x2D700182
+
+/* ===== NPC SOUNDS - CREATURES ===== */
+#define HT_Sound_SFX_NPC_CREATURE_BUTTERFLIES           0x2D700142
+
+/* ===== NPC SOUNDS - FRUCTOSE ===== */
+#define HT_Sound_SFX_NPC_FRUCTOSE_FART_LARGE            0x2D70009D
+#define HT_Sound_SFX_NPC_FRUCTOSE_FART_MEDIUM           0x2D70009C
+#define HT_Sound_SFX_NPC_FRUCTOSE_FART_SMALL            0x2D70009B
+
+/* ===== NPC SOUNDS - MANNY ===== */
+#define HT_Sound_SFX_NPC_MANNY_GROWL_SMALL              0x2D700161
+#define HT_Sound_SFX_NPC_MANNY_TRUNK_MOVEMENT           0x2D700160
+
+/* ===== NPC SOUNDS - RHINO ===== */
+#define HT_Sound_SFX_NPC_RHINO_EAT                      0x2D7001D7
+#define HT_Sound_SFX_NPC_RHINO_GROWL                    0x2D700167
+#define HT_Sound_SFX_NPC_RHINO_HIT_TREE                 0x2D7001D8
+#define HT_Sound_SFX_NPC_RHINO_IDLE                     0x2D700168
+#define HT_Sound_SFX_NPC_RHINO_STEP_L                   0x2D700169
+#define HT_Sound_SFX_NPC_RHINO_STEP_R                   0x2D70016A
+#define HT_Sound_SFX_NPC_RHINO_STEP_SCRAPE              0x2D7001D6
+#define HT_Sound_SFX_NPC_RHINO_STEP_SKID                0x2D7001D5
+
+/* ===== NPC SOUNDS - SHAMAN ===== */
+#define HT_Sound_SFX_NPC_SHAMAN_SHAKER                  0x2D70065D
+
+/* ===== NPC SOUNDS - SHORTNOSE ===== */
+#define HT_Sound_SFX_NPC_SHORTNOSE_STEP_L               0x2D700833
+#define HT_Sound_SFX_NPC_SHORTNOSE_STEP_R               0x2D700832
+
+/* ===== NPC SOUNDS - TWEETY BIRD ===== */
+#define HT_Sound_SFX_NPC_TWEETY_BIRD                    0x2D7008C1
+#define HT_Sound_SFX_NPC_TWEETY_BIRD_FLAP               0x2D7008C4
+#define HT_Sound_SFX_NPC_TWEETY_BIRD_LAND               0x2D7008C3
+
+/* ===== NPC SOUNDS - WALRUS ===== */
+#define HT_Sound_SFX_NPC_WALRUS_EAT                     0x2D7000E9
+#define HT_Sound_SFX_NPC_WALRUS_GROWL                   0x2D7000ED
+#define HT_Sound_SFX_NPC_WALRUS_STEP_L                  0x2D7000EB
+#define HT_Sound_SFX_NPC_WALRUS_STEP_R                  0x2D7000EC
+#define HT_Sound_SFX_NPC_WALRUS_SWALLOW                 0x2D7000EA
+#define HT_Sound_SFX_NPC_WALRUS_TAKEHIT                 0x2D7000E8
+
+/* ===== NPC SOUNDS - ZELDA (ALBINO PLATYPUS) ===== */
+#define HT_Sound_SFX_NPC_ZELDA_ATTACK                   0x2D7001BC
+#define HT_Sound_SFX_NPC_ZELDA_IDLE_PREEN               0x2D7001C1
+#define HT_Sound_SFX_NPC_ZELDA_PECK                     0x2D7001BB
+#define HT_Sound_SFX_NPC_ZELDA_SKID                     0x2D7001AA
+#define HT_Sound_SFX_NPC_ZELDA_STEP_L                   0x2D7001A9
+#define HT_Sound_SFX_NPC_ZELDA_STEP_LAND                0x2D7001C2
+#define HT_Sound_SFX_NPC_ZELDA_STEP_R                   0x2D7001A8
+#define HT_Sound_SFX_NPC_ZELDA_TAKEHIT                  0x2D7001C3
+#define HT_Sound_SFX_NPC_ZELDA_TALK                     0x2D7001BD
+#define HT_Sound_SFX_NPC_ZELDA_TALK_2                   0x2D7001BE
+#define HT_Sound_SFX_NPC_ZELDA_TALK_3                   0x2D7001BF
+#define HT_Sound_SFX_NPC_ZELDA_TALK_4                   0x2D7001C0
+#define HT_Sound_SFX_NPC_ZELDA_WING_FLAP                0x2D7001C4
+
+/* ===== OBJECT SOUNDS - GEYSER ===== */
+#define HT_Sound_SFX_OBJ_GEYSER_BLOCKED                 0x2D70004F
+#define HT_Sound_SFX_OBJ_GEYSER_ERUPT_LARGE             0x2D70006D
+#define HT_Sound_SFX_OBJ_GEYSER_ERUPT_MEDIUM            0x2D70006C
+#define HT_Sound_SFX_OBJ_GEYSER_ERUPT_SMALL             0x2D70006B
+#define HT_Sound_SFX_OBJ_GEYSER_PLUG                    0x2D700051
+#define HT_Sound_SFX_OBJ_GEYSER_UNBLOCKED               0x2D700050
+#define HT_Sound_SFX_OBJ_GEYSER_VENT_LOOP               0x2D700158
+#define HT_Sound_SFX_OBJ_GEYSER_VENT_LOOP_END           0x2D700159
+
+/* ===== OBJECT SOUNDS - GRASS/PLANTS ===== */
+#define HT_Sound_SFX_OBJ_GRASS_FLOWERS_MOVE             0x2D70010E
+#define HT_Sound_SFX_OBJ_GRASS_MOVE                     0x2D700042
+#define HT_Sound_SFX_OBJ_GRASS_PLANT_HITABLE_BASIC      0x2D700181
+#define HT_Sound_SFX_OBJ_GRASS_PLANT_LARGE_MOVE         0x2D70010D
+#define HT_Sound_SFX_OBJ_GRASS_PLANT_SMALL_MOVE         0x2D70010C
+
+/* ===== OBJECT SOUNDS - MISC ===== */
+#define HT_Sound_SFX_OBJ_LIGHTNING_BOLT                 0x2D700110
+
+/* ===== OBJECT SOUNDS - PICKUPS (GENERAL) ===== */
+#define HT_Sound_SFX_OBJ_PICKUP_BONUS                   0x2D700115
+#define HT_Sound_SFX_OBJ_PICKUP_BOOST_SWEETENER         0x2D700817
+#define HT_Sound_SFX_OBJ_PICKUP_FIND                    0x2D7001A4
+#define HT_Sound_SFX_OBJ_PICKUP_FRUIT                   0x2D700037
+#define HT_Sound_SFX_OBJ_PICKUP_PEBBLE                  0x2D700035
+#define HT_Sound_SFX_OBJ_PICKUP_PETAL                   0x2D700036
+#define HT_Sound_SFX_OBJ_PICKUP_PLUCK                   0x2D70003D
+
+/* ===== OBJECT SOUNDS - PICKUPS (DANDELION) ===== */
+#define HT_Sound_SFX_OBJ_PICKUP_DANDELION_GLOW          0x2D70015F
+#define HT_Sound_SFX_OBJ_PICKUP_DANDELION_SMASH         0x2D70015E
+
+/* ===== OBJECT SOUNDS - PICKUPS (GEMS) ===== */
+#define HT_Sound_SFX_OBJ_PICKUP_GEM_1                   0x2D70066E
+#define HT_Sound_SFX_OBJ_PICKUP_GEM_5                   0x2D70066F
+#define HT_Sound_SFX_OBJ_PICKUP_GEM_10                  0x2D700670
+#define HT_Sound_SFX_OBJ_PICKUP_GEM_10_CHILD            0x2D70080E
+#define HT_Sound_SFX_OBJ_PICKUP_GEM_10_CHILD_2          0x2D70080F
+
+/* ===== OBJECT SOUNDS - PICKUPS (ICE/FISH/NUT) ===== */
+#define HT_Sound_SFX_OBJ_PICKUP_ICE_BLOCK_SMASH         0x2D70009E
+#define HT_Sound_SFX_OBJ_PICKUP_FISH_LAND               0x2D700046
+#define HT_Sound_SFX_OBJ_PICKUP_NUT_LAND                0x2D700059
+#define HT_Sound_SFX_OBJ_PICKUP_NUT_SUCCESS             0x2D700045
+
+/* ===== OBJECT SOUNDS - PICKUPS (PEBBLE VARIANTS) ===== */
+#define HT_Sound_SFX_OBJ_PICKUP_PEBBLE_BOOST_CHIFF      0x2D700133
+#define HT_Sound_SFX_OBJ_PICKUP_PEBBLE_DISPENSE         0x2D700127
+#define HT_Sound_SFX_OBJ_PICKUP_PEBBLE_DISPENSE_BASIC   0x2D700135
+#define HT_Sound_SFX_OBJ_PICKUP_PEBBLE_HIT_ENEMY        0x2D700140
+#define HT_Sound_SFX_OBJ_PICKUP_PEBBLE_HIT_OBJECT       0x2D70013F
+#define HT_Sound_SFX_OBJ_PICKUP_PEBBLE_RATTLE           0x2D700134
+
+/* ===== OBJECT SOUNDS - PICKUPS (PHYSICS) ===== */
+#define HT_Sound_SFX_OBJ_PICKUP_PHYSICS_FISH_LAND       0x2D7001EE
+#define HT_Sound_SFX_OBJ_PICKUP_PHYSICS_FRUIT_LAND      0x2D7001ED
+#define HT_Sound_SFX_OBJ_PICKUP_PHYSICS_NUT_LAND        0x2D7001EF
+#define HT_Sound_SFX_OBJ_PICKUP_PHYSICS_ROCK_LAND       0x2D7001EC
+
+/* ===== OBJECT SOUNDS - PICKUPS (PLUGGING) ===== */
+#define HT_Sound_SFX_OBJ_PICKUP_PLUG_HOLE               0x2D70003F
+
+/* ===== OBJECT SOUNDS - PICKUPS (SPLASH) ===== */
+#define HT_Sound_SFX_OBJ_PICKUP_SPLASH                  0x2D70089A
+#define HT_Sound_SFX_OBJ_PICKUP_SPLASH_LAVA             0x2D7008D3
+#define HT_Sound_SFX_OBJ_PICKUP_SPLASH_MUD              0x2D7008D4
+
+/* ===== OBJECT SOUNDS - PICKUPS (SWEETENER/SPECIAL) ===== */
+#define HT_Sound_SFX_OBJ_PICKUP_TINKLE_SWEETENER        0x2D700164
+
+/* ===== OBJECT SOUNDS - STONE BLOCKS ===== */
+#define HT_Sound_SFX_OBJ_STONE_BLOCK_MOVE_LOOP          0x2D70004E
+#define HT_Sound_SFX_OBJ_STONE_BLOCK_MOVE_START         0x2D70004C
+#define HT_Sound_SFX_OBJ_STONE_BLOCK_MOVE_STOP          0x2D70004D
+
+#endif // HT_SOUND_SFX_2_H
